750bfa4615a002d43ea0d1b3af95821bd6088f33
shunshun <shuns3777@gmail.com>
2024-05-02 10:09:15 +0630

    add middleware

--- routes/web.php
+++ routes/web.php
@@ -9,13 +9,7 @@ Route::get('/', function () {
 Route::get('/' , function(){
     return view('home');
 });
-// Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-// Auth::routes();
-
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Auth::routes();
 
