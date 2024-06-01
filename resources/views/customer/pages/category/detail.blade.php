126588c8f7b4b4b621fde4de2252306f44950ea9
shunshun <shuns3777@gmail.com>
2024-06-01 18:57:18 +0630

    update

--- resources/views/customer/pages/category/detail.blade.php
+++ resources/views/customer/pages/category/detail.blade.php
@@ -2,85 +2,7 @@
 @section('title','ProductDetails')
 @section('content')
 
-    <div class="section1">
-        <!-- navi -->
-        <div class="navigation flex_col">
-            <div class="flash_sale_bar flex_row">
-                <div class="flash_sale">
-                    Flash Sales : Sign in and Get Extra  25%  off on Selected Items
-                </div>
-                <div class="link">
-                    <a href="">FAQ</a>|
-                    <a href="">orders and returns</a>|
-                    <a href="{{url('/customer/login')}}">Sign In</a>
-                </div>
-            </div>
-            <div class="nav_bar flex_row">
-                <div class="logo">
-                    Bravis
-                </div>
-                <div class="menu flex_row">
-                    <div class="women drop_down">
-                        <a href="{{url('/customer/product/women')}}" >Women</a>
-                        {{-- <div class="women_drop_down_content">
-                            <h3>Women Clothing</h3>
-                            <div class="women_clothing_list flex_row">
-                                <div class="flex_col">
-                                    <a href="../women/dress.html">Women's Dresses</a>
-                                    <a href="../women/blouse.html">Women's Tops, Tees & Blouses</a>
-                                    <a href="../women/hoodie.html">Women's Fashion Hoodies & Sweat shirts</a>
-                                    <a href="../women/pant.html">Women's Pants</a>
-                                    <a href="../women/skirt.html">Women's Skirts</a>
-                                </div>
-                            </div>
-                        </div> --}}
-                    </div>
-                    <div class="men drop_down">
-                        <a href="{{url('/customer/product/men')}}" >Men</a>
-                        {{-- <div class="men_drop_down_content">
-                            <h3>Men Clothing</h3>
-                            <div class="men_clothing_list flex_row">
-                                <div class="flex_col">
-                                    <a href="../men/tee.html">Men's Tee</a>
-                                    <a href="../men/men_t_shirt.html">Men's T-Shirts</a>
-                                    <a href="../men/hoodie.html">Men's Hoodies & Sweat Shirts</a>
-                                    <a href="../men/pant.html">Men’s Pants</a>
-                                </div>
-                            </div>
-                        </div> --}}
-                    </div>
-                    <div class="contact">
-                        <a href="../../contact/index.html" >Contact</a>
-                    </div>
-                    <div class="about_us">
-                        <a href="../../about/index.html" >About Us</a>
-                    </div>
-                </div>
-                <div class="extra_icon flex_row">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                    <div class="add_to_cart_icon">
-                        <i class="fa-solid fa-cart-shopping"></i>
-                        <span>0</span>
-                    </div>
-                </div>
-            </div>
-        </div>
-        {{-- <div class="navi_ph flex_row">
-            <div class="logo">
-                Bravis
-            </div>
-            <i class="fa-solid fa-bars hamburger_menu open_menu"></i>
-            <i class="fa-solid fa-xmark close_menu"></i>
-        </div>
-        <div class="overflow_menu">
-            <div class="menu_link flex_col">
-                <a href="">Women</a>
-                <a href="">Men</a>
-                <a href="/pages/contact/index.html">Contact</a>
-                <a href="/pages/about/index.html">About</a>
-            </div>
-        </div> --}}
-    </div>
+
     
     <div class="img">
     <div class="shopping_cart_box">
@@ -159,20 +81,6 @@
                 <p>The Bear "Fashion Icon" Tee</p>
                 <p>25000MMK</p> 
                 </div>
-        </div>
-    </div>
-    <div class="footer grid">
-        <div class="flex_col">
-            <h3><a href="{{url('/customer/homepage')}}">Home</a></h3>
-        </div>
-        <div class="flex_col">
-            <h3><a href="">About Us</a></h3>
-        </div>
-        <div class="flex_col">
-            <h3><a href="">Contact Us</a></h3>
-        </div>
-        <div class="flex_col">
-            <h3><a href="{{url('/cutomer/product/list')}}">All Products</a></h3>
         </div>
     </div>
 
